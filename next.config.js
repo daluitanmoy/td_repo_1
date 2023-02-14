d8da4bdba266aa1f666a5a15dcbf3d469356e75f
Tanmoy Azure <tdalui.azure@gmail.com>
2023-02-14 10:23:00 -0600

    Updated

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/td_repo_1/" : "",
+  // assetPrefix: isProd ? "/td_repo_1/" : "",
+  basePath: "",
+  assetPrefix: "/td_repo_1/",
   /**
    * Disable server-based image optimization.
    *
